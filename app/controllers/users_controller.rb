556ee2cf03b8167352d2238629f1659d18e77eca
Vuong Tran <lifebreakvn@gmail.com>
2015-12-14 10:39:14 +0000

    style and migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   end
   
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214065032) do
+ActiveRecord::Schema.define(version: 20151214071842) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "body"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151214065032) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "user_name"
   end
 
 end
